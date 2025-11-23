c410f653eee83912df92b2526756f559f3561a41
Steve <falecomstevevaz@gmail.com>
2025-11-23 10:22:44 -0300

    White space function remove strip()

---
+++ introduction/RemovingWhiteSpaces.py
@@ -0,0 +1,8 @@
+favoriteLanguage = " Python "
+print(favoriteLanguage)
+
+""""" favoriteLanguage = favorite.rstrip() """""
+favoriteLanguage = favoriteLanguage.rstrip()
+favoriteLanguage = favoriteLanguage.lstrip()
+favoriteLanguage = favoriteLanguage.strip()
+print(favoriteLanguage)
