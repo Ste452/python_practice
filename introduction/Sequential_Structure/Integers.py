8b7302cdcefbce940642694ee25182941e325238
Steve <falecomstevevaz@gmail.com>
2025-11-23 10:51:26 -0300

    Integers

---
+++ introduction/Sequential_Structure/Integers.py
@@ -0,0 +1,20 @@
+
+x = 3.0
+y = 2
+
+divisionIntegers = x / y
+
+print(divisionIntegers) # 1
+
+""" Exercise: Addition, Subtraction, Multiplication and Division result the number 8 """
+
+print(5+3)
+print(9-1)
+print(2*4)
+print(int(72/9))
+
+favorite_number = 7
+message = "My favorite number is " + str(favorite_number) + " because it is a prime number and has unique divisibility."
+
+print(message)
+
