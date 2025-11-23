6d6759f5ef405dc700851ba16bf7b736d7422911
Steve <falecomstevevaz@gmail.com>
2025-11-23 11:14:46 -0300

    Comments in Python and Zen Python

---
+++ introduction/Sequential_Structure/Comments.py
@@ -0,0 +1,7 @@
+# Ignores the first line and second line explain what will be occur
+# Output: Hello Python people!
+print("Hello Python people!")
+
+# Zen Python
+# It presents a series of philosophies for writing good code in programming.
+import this
