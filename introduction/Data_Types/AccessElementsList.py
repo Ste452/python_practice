f532529c4abe40949b8cb7722fe537032ea8be78
Steve <falecomstevevaz@gmail.com>
2025-11-23 11:35:26 -0300

    Access Elements List and print on terminal

---
+++ introduction/Data_Types/AccessElementsList.py
@@ -0,0 +1,10 @@
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+print(bicycles[0].title())
+print(bicycles[1].title())
+print(bicycles[2].title())
+print(bicycles[-1].title())
+
+message = "My first bicycle way a " + bicycles[0].title() + "."
+print(message)
---
+++ introduction/Data_Types/exercises/NamesList.py
@@ -0,0 +1,8 @@
+names = ["bob", "joey", "jill"]
+bobMessage = "Hello " + names[0].title() + "!"
+joeyMessage = "Hello " + names[1].title() + "!"
+jillMessage = "Hello " + names[2].title() + "!"
+
+print(bobMessage)
+print(joeyMessage)
+print(jillMessage)
