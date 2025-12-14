3b8ace8b201ef3ce32378cb10af785cb5d88e143
Steve <falecomstevevaz@gmail.com>
2025-12-14 00:09:19 -0300

    Reversing Sort List

--- introduction/Data_Types/sortedList.py
+++ introduction/Data_Types/sortedList.py
@@ -1,9 +1,18 @@
 cars = ['bmw', 'audi', 'toyota, subaru']
 
-# Alphabetic order
+print("\nHere is the original list:")
+print(cars)
+
+# Alphabetical order
+print("\nHere is the sorted list:")
 cars.sort()
 print(cars)
 
-# Reverse alphabetic order
+# Reversed alphabet sorted
+print("\nHere is the Reverse alphabetic sorted list:")
 cars.sort(reverse=True)
+print(cars)
+
+# Reverses the sort of the list
+cars.reverse()
 print(cars)
