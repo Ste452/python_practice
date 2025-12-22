350c5172f66b3f4cb82a9e17a0b5347bc31aeef7
Steve <falecomstevevaz@gmail.com>
2025-12-22 00:02:40 -0300

    For range and Numbers list

---
+++ introduction/Data_Types/Loops/for/NumbersList.py
@@ -0,0 +1,13 @@
+numbers = list(range(1, 6))
+print(numbers)
+
+evenNumbers = list(range(2,11,2))
+print(evenNumbers)
+
+squares = []
+for value in range(1, 11):
+    
+    square = value**2
+    squares.append(square)
+    
+print(squares)
