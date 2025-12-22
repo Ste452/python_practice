3370fc2f257a066044ee5b56ece139ff2a4c8ec6
Steve <falecomstevevaz@gmail.com>
2025-12-21 22:14:35 -0300

    for exercise one

---
+++ introduction/Data_Types/Loops/exercises/Pizzas.py
@@ -0,0 +1,13 @@
+"""""
+1° - Exercise: Pizzas
+
+Pizzas: Think of at least three favorite types of pizza. 
+Store the names of these pizzas and then use a for loop to display the name of each pizza.
+    
+"""""
+
+pizzas_List = ['Mozzarella', 'Pepperoni', 'Neapolitan']
+
+for pizza in pizzas_List:
+    
+    print("\n\tAvailable Pizza: " + pizza.title() + "\n")
